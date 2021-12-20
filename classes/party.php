b1e4bdbff043880b08dae6d8fa407419c0e34958
Republican54 <jay@vanhorik.com>
2021-12-20 19:57:41 +0100

    Begin gemaakt aan CRUD. Create is gemaakt. -JAY

--- classes/party.php
+++ classes/party.php
@@ -1,7 +1,25 @@
 <?php
+//include("Connection.php");
+//include ("../webpage/create_party.php");
 
-
+if(isset($_POST['create'])){
+    //$msg =
+}
 class party
 {
+function Create(){
+    $partyName = $_POST['name'];
+    $partyEstablished = $_POST['established'];
+    $partyLeader = $_POST['party_leader'];
+    $imgLogo = $_POST['img'];
+
+    $query = "INSERT INTO party(name, established, party_leader, img) VALUES('$partyName', '$partyEstablished', '$partyLeader', '$imgLogo')";
+    $exec = mysqli_query($conn, $query);
+    if($exec){
+        $msg = "Data was created successfully";
+        return $msg;
+    }
+}
+
 
 }
